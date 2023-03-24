8120741ace8bc658e182fb1d422749ee8af1306d
Yuval <46052720+supraBlatt@users.noreply.github.com>
2023-03-25 01:06:32 +0300

    IT'S SO SLOW WTF

--- parsort-0/parsort.cpp
+++ parsort-0/parsort.cpp
@@ -4,6 +4,7 @@
 #include <omp.h>
 #include <algorithm>
 #include <chrono>
+#define THRESHOLD 1000
 
 std::vector<std::uint64_t> load_file_contents (const std::string& path) { 
 
@@ -25,9 +26,22 @@ std::vector<std::uint64_t> load_file_contents (const std::string& path) {
   return vec;
 }
 
-void parallel_mergesort(std::vector<std::uint64_t>& vec, int left, int right) {
+void mergesort(std::vector<std::uint64_t>& vec, int left, int right) {
   // serial sections
 	if (right <= left) { return; }
+  auto middle = (right + left)/2;
+
+  mergesort(vec, left, middle);
+  mergesort(vec, middle+1, right);
+
+  // merge the two sorted halves
+  auto begin = vec.begin();
+  std::inplace_merge(begin + left, begin + middle + 1, begin + right + 1); 
+}
+
+void parallel_mergesort(std::vector<std::uint64_t>& vec, int left, int right) {
+  // serial sections
+	if ((right - left) <= THRESHOLD) { mergesort(vec, left, right); return; }
   auto middle = (right + left)/2;
 
   // make two tasks, one for each half
@@ -54,15 +68,27 @@ void parallel_sort(std::vector<uint64_t>& vec, int num_threads) {
   }
 }
 
-int main() {
-  auto numbers = load_file_contents("test-files/input.txt");  
+int main(int argc, char** argv) {
+  // used as 'parsort [number of processors to use] [input file to sort]'
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " [num_processors] [input_file_path]" << std::endl;
+    return 1;
+  }
+  
+  // parse the command-line arguments
+  int num_threads = std::stoi(argv[1]);
+  std::string input_file_name{argv[2]};
 
+  auto numbers = load_file_contents(input_file_name);  
+
+  // time the sort. spoiler: it's pretty bad
   auto start_time = std::chrono::high_resolution_clock::now();
-  parallel_sort(numbers, 2);
+  parallel_sort(numbers, num_threads);
   auto end_time = std::chrono::high_resolution_clock::now();
   auto duration_us = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
-    
+
+  // print [sort name] [time] \n [sorted array]
   std::cout << "Merge Sort " << duration_us.count() << std::endl;
-  for (auto e: numbers) { std::cout << e << " "; }
+  //for (auto e: numbers) { std::cout << e << " "; }
   return 0;
 }
