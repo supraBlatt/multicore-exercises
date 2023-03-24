7b9a3fe8ec959dce96e56b8b9824c7e1d62b5689
Yuval <46052720+supraBlatt@users.noreply.github.com>
2023-03-25 00:08:21 +0300

    lost 7 hours of commits. nothing works.

--- parsort-0/parsort.cpp
+++ parsort-0/parsort.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <omp.h>
+#include <algorithm>
 
 std::vector<std::uint64_t> load_file_contents (const std::string& path) { 
 
@@ -22,6 +24,38 @@ std::vector<std::uint64_t> load_file_contents (const std::string& path) {
   return vec;
 }
 
+void parallel_mergesort(std::vector<std::uint64_t>& vec, int left, int right) {
+  if ((right-left) <= 0) {
+    return;
+  }
+
+  auto middle = (right + left)/2;
+
+  // make two tasks, one for each half
+  #pragma omp task
+  parallel_mergesort(vec, left, middle);
+  #pragma omp task
+  parallel_mergesort(vec, middle+1, right);
+  // sync the tasks
+  #pragma omp taskwait
+
+  // merge the two sorted halves
+  auto begin = vec.begin();
+  std::inplace_merge(begin + left, begin + middle, begin + right); // still using as a crutch
+}
+
+void parallel_sort(std::vector<uint64_t>& vec, int num_threads) {
+  // limit the number of threads
+  // omp_set_num_threads(num_procs);
+
+  // spawn big boi thread army
+  // let only one thread start the forking process
+  #pragma omp parallel num_threads(num_threads)
+  {
+    #pragma omp single nowait 
+    parallel_mergesort(vec, 0, vec.size() - 1);
+  }
+}
 
 int main() {
   auto numbers = load_file_contents("test-files/input.txt");  
