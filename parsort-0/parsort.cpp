fbd0b13eae8f899a7091371001893fd82c78d1e9
Yuval <46052720+supraBlatt@users.noreply.github.com>
2023-03-24 16:14:53 +0300

    added input file & reading from input

--- parsort-0/parsort.cpp
+++ parsort-0/parsort.cpp
@@ -1,16 +1,35 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
-#include <string>
 
-using namespace std;
+std::vector<std::uint64_t> load_file_contents (const std::string& path) { 
 
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+  std::vector<std::uint64_t> vec{};
+  std::ifstream file{path};
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+  if (!file.is_open()) {
+    // handle the error if the file could not be opened
+    std::cerr << "Error: could not open file." << std::endl;
+    return {};
+  }
+
+  // read the numbers from the file
+  std::uint64_t num;
+  while (file >> num) {
+    vec.push_back(num);
+  }
+
+  return vec;
+}
+
+
+int main() {
+  auto numbers = load_file_contents("test-files/input.txt");  
+  std::cout << numbers.size();
+
+  // sort the numbers
+  // sort 
+
+  // print timed results
+  return 0;
 }
